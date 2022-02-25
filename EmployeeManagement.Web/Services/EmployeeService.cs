586b35ec96ad18dcd4ae1a3626fe615a44802fda
Juan Daniel Pérez Sevilla <jdanielsevilla@outlook.es>
2022-02-24 19:03:28 -0600

    First commit

---
+++ EmployeeManagement.Web/Services/EmployeeService.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagement.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly HttpClient httpClient;
+
+        public EmployeeService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<Employee> GetEmployee(int id)
+        {
+            return await httpClient.GetFromJsonAsync<Employee>($"api/employees/{id}");
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await httpClient.GetFromJsonAsync<Employee[]>("api/employees");
+        }
+    }
+}
